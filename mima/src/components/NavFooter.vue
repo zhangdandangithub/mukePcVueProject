8603d74dc4a02cf290668cb35546bc2f5bd8f000
代码如诗 <weiheli@vip.qq.com>
2020-05-30 02:05:42 +0800

    first

---
+++ mima/src/components/NavFooter.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+  export default {
+    name: "nav-footer"
+  }
+</script>
+
+<style lang="less" scoped>
+
+</style>
---
+++ mima/src/components/NavHeader.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+  export default {
+    name: "nav-header"
+  }
+</script>
+
+<style lang="less" scoped>
+
+</style>
